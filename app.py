873e914057c9a29e51bbaaf62d79a5f45b020fa4
Hallel <halleli100@gmail.com>
2021-10-12 15:40:40 +0300

    change names

--- app.py
+++ app.py
@@ -8,4 +8,11 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Hallel Python Princess</h1>"
 
+
+@app.route('/alert', methods=['GET'])
+def create_alert():
+
+    return "nothing but alert"
+
+
 app.run()
